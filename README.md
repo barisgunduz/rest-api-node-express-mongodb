ae56850f4f72743c69b347da1b2a63d033166867
Barış Gündüz <baris.gunduz@devstup.org>
2021-10-05 22:30:25 +0300

    readme

---
+++ README.md
@@ -0,0 +1,3 @@
+### Rest API Sample
+/subscribers
+- GET & POST & DELETE & PATCH
--- server.js
+++ server.js
@@ -4,15 +4,19 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+// DB Connection
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
-
 const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("connected to db"));
 
+// JSON
 app.use(express.json())
 
+// Routes
 const subscribersRouter = require('./routes/subscribers')
+
 app.use('/subscribers', subscribersRouter)
+
 
 app.listen(3000, () => console.log("Server started"));
